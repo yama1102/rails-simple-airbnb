bcd168c3db10ba5c07458698e394308e0cc70a40
Tsubasa Yamauchi <tsubasa.yamauchi1102@gmail.com>
2021-11-17 17:04:46 -0300

    first commit

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,6 @@
+class FlatsController < ApplicationController
+
+  def index
+    @flats = Flat.all
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+4.times do
+  Flat.create!(
+    name: Faker::Company.name,
+    address: Faker::Address.street_address,
+    description: Faker::Restaurant.description,
+    price_per_night: rand(100..500),
+    number_of_guests: rand(1..5)
+  )
+end
